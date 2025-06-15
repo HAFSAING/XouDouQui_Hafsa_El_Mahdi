609338116ab395999fa81bcf31c358536bcdcfef
HAFSAING <hafsaelmahdi.in@gmail.com>
2025-06-15 04:16:19 +0100

    6

--- jungle-game/src/main/java/jungle/Main.java
+++ jungle-game/src/main/java/jungle/Main.java
@@ -1,9 +1,5 @@
 package jungle;
 
-/**
- * Point d'entrée de l'application Mini-Projet Java.
- * Crée le contrôleur de jeu et lance l'application.
- */
 public class Main {
     public static void main(String[] args) {
         GameController controller = new GameController();
